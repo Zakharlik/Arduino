2d1f3341f15bb9ca5dae453bdd78d911d017b654
Zakhar E. Likay <zakhar.likay@mail.ru>
2022-04-16 19:21:02 +0700

    New versoin of WaterPump Add: 	Dynamic max_water_pump 	Comments Fixed: 	Short and long button press conflict

--- WaterPump/WaterPump.ino
+++ WaterPump/WaterPump.ino
@@ -15,8 +15,10 @@
 
 
 #define WATER 200             //Measure to determine if water
-#define MAX_MOTOR_TIME 1500000 //Max motor working time in milliseconds
+#define MAX_MOTOR_TIME 40 //Max motor working time in minutes
 #define MEASURE_PERIOD 2000   //Period betwin measures
+
+int max_motor_time = 40;
 
 bool pump = false;
 bool was_high = false;
@@ -48,30 +50,33 @@ void setup()
 
 void loop()
 {
-  if (!digitalRead(BTN) && millis() - btn_t > 600) {
+  if (!digitalRead(BTN) && millis() - btn_t > 600) { //Turning pump on if button pressed long
     turnPumpOn();
     btn_t = millis();
   }
-
-  if (!digitalRead(BTN) && millis() - btn_t > 300) {
-    was_high = !was_high;
-    digitalWrite(LED_WASH, was_high);
-    btn_t = millis();
+  else {
+    if (!digitalRead(BTN) && millis() - btn_t > 300) {  //Swich was_high status if button pressed short
+      was_high = !was_high;
+      digitalWrite(LED_WASH, was_high);
+      btn_t = millis();
+    }
+    
   }
-  if (millis() - measure_time > MEASURE_PERIOD) {
+  
+  if (millis() - measure_time > MEASURE_PERIOD) { //Measure water level
     digitalWrite(VCCM_PIN, LOW);
     delay(50);
     low_sensor = 1023 - analogRead(LOW_PIN);
     high_sensor = 1023 - analogRead(HIGH_PIN);
     digitalWrite(VCCM_PIN, HIGH);
     measure_time = millis();
-    if (low_sensor > WATER) {
+    if (low_sensor > WATER) { //If water higher than low sensor - turn on low LED
       digitalWrite(LED_LOW, HIGH);
     } else {
       digitalWrite(LED_LOW, LOW);
 
     }
-    if (high_sensor > WATER) {
+    if (high_sensor > WATER) { //If water higher than high sensor - turn on high LED
       digitalWrite(LED_HIGH, HIGH);
     } else {
       digitalWrite(LED_HIGH, LOW);
@@ -79,25 +84,28 @@ void loop()
     }
 
 
-    if (!pump && was_high && low_sensor < WATER) {
+    if (!pump && was_high && low_sensor < WATER) { //If pump off and water was high and now water is veri low then turn on the pump
       turnPumpOn();
     }
-    if (high_sensor > WATER) {
+
+    if (high_sensor > WATER) { //If water upper than high sensor - turn pump off
       pump = false;
       was_high = true;
       digitalWrite(LED_WASH, HIGH);
+      max_motor_time = max_motor_time * 0.95 + (millis() - motor_timer) * 0.11;
 
     }
-    if (millis() - motor_timer > MAX_MOTOR_TIME) {
+    if (millis() - motor_timer > max_motor_time * 60 * 1000) { //If timer expired - turn pump off
       pump = false;
     }
-    if (pump) {
-      digitalWrite(RELLAY_PIN, HIGH);
+    if (pump) {                           //turn pump ON and OFF
+      digitalWrite(RELLAY_PIN, HIGH); 
     }
     else {
       digitalWrite(RELLAY_PIN, LOW);
     }
 
+/*  // Debugging
     Serial.print(high_sensor);
     Serial.print(',');
     Serial.print(low_sensor);
@@ -109,9 +117,10 @@ void loop()
     Serial.print(was_high);
     Serial.print(',');
     Serial.println(pump);
+*/
   }
-  if (pump) {
-    i = (byte)((MAX_MOTOR_TIME - (millis() - motor_timer)) / 60000);
+  if (pump) { //Blink Motor LED
+    i = (byte)((max_motor_time * 60 * 1000 - (millis() - motor_timer)) / 60000);
     if (k == 0) {
       if (blink_flag) {
         blink_flag = false;
